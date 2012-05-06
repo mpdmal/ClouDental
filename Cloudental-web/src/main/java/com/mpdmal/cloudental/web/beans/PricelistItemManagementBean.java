9dae92df77b6e515d67cfd8518a81c43c40a1966
Gerassimos Mitropoulos <gerassimos@gmail.com>
2012-05-06 19:56:25 +0300

    Price List CRUD operations via web

--- Cloudental-web/src/main/java/com/mpdmal/cloudental/web/beans/PricelistItemManagementBean.java
+++ Cloudental-web/src/main/java/com/mpdmal/cloudental/web/beans/PricelistItemManagementBean.java
@@ -1,17 +1,16 @@
 package com.mpdmal.cloudental.web.beans;
 
-import java.util.Vector;
+import java.util.Collection;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
-import com.mpdmal.cloudental.beans.DentistBean;
 import com.mpdmal.cloudental.beans.DentistServices;
 import com.mpdmal.cloudental.beans.PatientServices;
 import com.mpdmal.cloudental.entities.Dentist;
 import com.mpdmal.cloudental.entities.PricelistItem;
-import com.mpdmal.cloudental.util.exception.DentistNotFoundException;
-import com.mpdmal.cloudental.util.exception.InvalidPostitAlertException;
+import com.mpdmal.cloudental.util.exception.PricelistItemNotFoundException;
 
 public class PricelistItemManagementBean extends PricelistItem {
 	
@@ -23,41 +22,51 @@ public class PricelistItemManagementBean extends PricelistItem {
 	DentistServices dentistService;
 	@EJB
 	PatientServices patientServices;
-
-	@EJB
-	DentistBean dentistBean;
 	
-	public Vector<PricelistItem> getPricelist() {
+	private Collection<PricelistItem> pricelistCol;
+	
+	private PricelistItem item ;
+	
+	public Collection<PricelistItem> getPricelist() {
 		Dentist d = user.getCurrentUser();
-		System.out.println(""+d.getName()+d.getId());
-		
-		Vector<PricelistItem>  ans  = new Vector<PricelistItem>();
-		for (PricelistItem item : d.getPriceList()) {
-			ans.add(item);
+		System.out.println("getPricelist()");
+		if(pricelistCol==null){
+			System.out.println("call  service for:"+d.getName()+d.getId());
+			pricelistCol = dentistService.getPricelist(d.getId());
 		}
-		return  ans;
+		
+		return pricelistCol;
+		
 	}
 
 	public String createPricelistItem(){
-		System.out.println("create: "+getTitle());
+		System.out.println("create PriceListItem: "+getTitle());
 		try {
 			dentistService.createPricelistItem(user.getCurrentUser().getId(), getTitle(),getDescription(), getPrice().doubleValue() );
-		} catch (InvalidPostitAlertException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (DentistNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} 
 		
 		return null;
 	}
 
 	public String updatePricelistItem(){
-		System.out.println("update: "+getTitle());
+		System.out.println("update PricelistItem: "+getTitle());
+//		dentistService.updatePricelistItem();
+		return null;
+	}
+	public String deletePricelistItem(){
+		System.out.println("delete PricelistItem: "+getSelectedItem().getTitle());
+		try {
+			dentistService.deletePricelistItem(getSelectedItem().getId() );
+			pricelistCol = dentistService.getPricelist(user.getCurrentUser().getId());
+		} catch (PricelistItemNotFoundException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
+	
 	public void setCurrentPricelistItem(PricelistItem currentItem){
 		System.out.println("setCurrentPricelistItem: "+currentItem.getTitle());
 		setId(currentItem.getId());
@@ -65,7 +74,47 @@ public class PricelistItemManagementBean extends PricelistItem {
 		setPrice(currentItem.getPrice());
 		setDescription(currentItem.getDescription());
 	}
+	
+	
+	public void rowEditListener(org.primefaces.event.RowEditEvent ev){
+		System.out.println("PricelistItemManagementBean rowEditListener()");
+        try {
+        	PricelistItem pricelistItem = (PricelistItem)ev.getObject();
+            //CarJpaController ctrlCar = new CarJpaController();
+            
+            if(pricelistItem == null) {
+                System.out.println("PricelistItem is null...");
+            } else {
+                System.out.println("PricelistItem is not null with id = " + pricelistItem.getId());
+                System.out.println( pricelistItem.getTitle());
+                System.out.println( pricelistItem.getDescription());
+                
+            }
+            if(pricelistItem.getTitle().equalsIgnoreCase("")) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "No streetname", null));
+            } else {
+            	
+                dentistService.updatePricelistItem(pricelistItem.getId(),pricelistItem.getDescription() ,pricelistItem.getTitle());
+                
+            }
+            
+        } catch (Exception e) {
+            System.out.println("rowEditListener ERROR = " + e.getMessage());
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERRORE", e.toString()));
+        }        
+    }
 
-
+    public PricelistItem getSelectedItem() {  
+    	if(item!=null)
+    		System.out.println("getSelectedItem: "+item.getTitle());
+    	else
+    		System.out.println("getSelectedItem: is null");
+    		
+        return item;  
+    }  
+    public void setSelectedItem(PricelistItem item) {  
+    	System.out.println("setSelectedItem: "+item.getTitle());
+        this.item = item;  
+    }  
 
 }
