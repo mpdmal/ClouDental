df3d01aeb26bd2f70f3076899c7ef2183881611c
arilou <arilou@MePeeCee>
2012-10-14 23:55:25 +0300

    report generator external tool need full jasper and postgres.jdbc4  jars

---
+++ CloudentalUtils/src/com/mpdamal/cloudental/external/reports/ApplicationFrame.java
@@ -0,0 +1,102 @@
+package com.mpdamal.cloudental.external.reports;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import javax.swing.JButton;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+public class ApplicationFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JPanel jpActions;
+	private JButton jbSingle;
+	private JButton jbDoAll;
+	private JTable _dentistTable;
+
+	public ApplicationFrame() {
+		setTitle("Daily Report Generator Tool");
+		setSize(500,500);
+		setLocation(150, 150);
+		initGrcs();
+		populateTable();
+	}
+	
+	private void initGrcs() {
+		GridBagLayout thisLayout = new GridBagLayout();
+		thisLayout.rowWeights = new double[] {0.1, 0.1};
+		thisLayout.rowHeights = new int[] {7, 7};
+		thisLayout.columnWeights = new double[] {0.1, 0.1, 0.1};
+		thisLayout.columnWidths = new int[] {20, 7, 7};
+		getContentPane().setLayout(thisLayout);
+
+		_dentistTable = new JTable();
+		getContentPane().add(new JScrollPane(_dentistTable), new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));		
+
+		jpActions = new JPanel();
+		{
+			jbDoAll = new JButton();
+			jbDoAll.setText("Create All Reports");
+			jbDoAll.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					for (int i = 0; i < _dentistTable.getRowCount(); i++) {
+						int dentistid = Integer.parseInt(_dentistTable.getValueAt(i, 0).toString());
+						DailyReportsGeneratorTool.printReport(dentistid);
+					}
+				}
+			});
+			jbSingle = new JButton();
+			jbSingle.setText("Create For this Patient");
+			jbSingle.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int dentistid = Integer.parseInt(_dentistTable.getValueAt(_dentistTable.getSelectedRow(), 0).toString());
+					DailyReportsGeneratorTool.printReport(dentistid);
+				}
+			});
+			jpActions.add(jbSingle);
+			jpActions.add(jbDoAll);
+		}
+		getContentPane().add(jpActions, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+	}
+	
+	private void populateTable() {
+		Vector<Vector<String>> data = new Vector<Vector<String>>();
+		Vector<String> row = new Vector<String>();
+		
+		try {
+			ResultSet rs = DailyReportsGeneratorTool.getDentistsInfo();
+			while ( rs.next() )	{
+				row = new Vector<String>(4);
+				row.add(rs.getString(1));
+				row.add(rs.getString(2));
+				row.add(rs.getString(3));
+				row.add(rs.getString(4));
+				data.add(row);
+			}
+			rs.getStatement().close();
+			rs.close();
+		} catch (SQLException se) {
+			System.err.println("Threw a SQLException creating the list of Dentist.");
+			System.err.println(se.getMessage());
+		}		
+		row = new Vector<String>(4);
+		row.add("ID");
+		row.add("Username");
+		row.add("Surname");
+		row.add("Name");
+		TableModel patientsTableModel = new DefaultTableModel(data, row);
+		_dentistTable.setModel(patientsTableModel);
+	}
+}
---
+++ CloudentalUtils/src/com/mpdamal/cloudental/external/reports/DailyReportsGeneratorTool.java
@@ -0,0 +1,151 @@
+package com.mpdamal.cloudental.external.reports;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.util.FileResolver;
+
+public class DailyReportsGeneratorTool {
+    public static final String ROOT_DIR = System.getProperty("user.home")+"/cdent/";
+    public static final String REPORTS_RELATIVEDIR = "reports/";
+    public static final String RESOURCES_RELATIVEDIR = "resources/";
+    public static final String PDF_NAME = "daily_report_$.pdf";
+    
+    public static String _dailyreport_compiled = "daily_report.jasper";
+    private static String _dbstring = "jdbc:postgresql://localhost:5432/CloudentDB",
+    						_dbuname = "cloudent", _dbpwd = "cloudent";
+    
+    private static boolean _launchUI = false;
+    private static File _compiledreport = null;
+    private static Connection _conn = null;
+    
+    //MAIN
+	public static void main(String[] args) {
+		for (String arg : args) {
+			if (arg.startsWith("-dbstr=")) {
+				_dbstring = arg.substring(7, arg.length());
+			} else
+			if (arg.startsWith("-dbuname=")) {
+				_dbuname = arg.substring(9, arg.length());
+			} else
+			if (arg.startsWith("-dbpwd=")) {
+				_dbpwd = arg.substring(7, arg.length());
+			} else			
+			if (arg.startsWith("-jasper=")) {
+				_dailyreport_compiled = arg.substring(8, arg.length());
+			} else				
+			if (arg.equals("-ui")) {
+				_launchUI = true;
+			}
+		}
+		setupConnection();
+		checkForJasper();
+
+		if (_launchUI) {
+			ApplicationFrame f = new ApplicationFrame();
+			f.setVisible(true);
+		} else {
+			autoGenerateReports();
+		}
+	}
+	
+	//PRIVATE
+	public static ResultSet getDentistsInfo() throws SQLException {
+		Statement st;
+		try {
+			st = _conn.createStatement();
+			ResultSet rs = st.executeQuery("SELECT id, username, surname, name FROM Dentist ORDER BY id");
+			return rs;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	
+	private static void autoGenerateReports() {
+		try {
+			ResultSet rs = getDentistsInfo();
+			while ( rs.next() )	{
+				printReport(rs.getInt(1));//dentistid column
+			}
+			rs.getStatement().close();
+			rs.close();
+			_conn.close();
+		} catch (SQLException se) {
+			System.err.println("Threw a SQLException creating the list of Dentist.");
+			System.err.println(se.getMessage());
+		}				
+	}
+	private static void checkForJasper() {
+		//check the reports dir is where its supposed to be ...
+		File f = new File(ROOT_DIR+REPORTS_RELATIVEDIR);
+		if (f.exists() && f.isDirectory()) {
+			System.out.println("reports dir found");
+		} else {
+			System.out.println("reports dir NOT found, exiting");
+			System.exit(0);
+		}
+		
+		//check the jasper compiled report (.jasper) is where its supposed to be ... 
+		_compiledreport = new File(ROOT_DIR+_dailyreport_compiled);
+		if (!_compiledreport.exists() || !_compiledreport.isFile()) {
+			System.out.println("daily compiled report ("+_dailyreport_compiled+") NOT found, exiting");
+			System.exit(0);
+		}
+	}
+	private static void setupConnection() {
+		//try connection to postgres
+		try {
+			Class.forName("org.postgresql.Driver");
+			_conn = DriverManager.getConnection(_dbstring, _dbuname, _dbpwd);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		System.out.println("Connection to postgres established :"+_dbstring);
+	}
+	
+	public static void printReport(int dentistid) {
+		try {
+			FileResolver fileResolver = new FileResolver() {
+				@Override
+				public File resolveFile(String fileName) {
+					return new File(ROOT_DIR+RESOURCES_RELATIVEDIR+fileName);
+				}
+			};
+			HashMap<String, Object> parameters = new HashMap<String, Object>();
+			parameters.put("REPORT_FILE_RESOLVER", fileResolver);
+			parameters.put("DENTISTID", new Integer(dentistid));
+			
+			JasperPrint jprint = JasperFillManager.fillReport(
+					new FileInputStream(_compiledreport),
+					parameters, _conn);
+			String outname = ROOT_DIR+REPORTS_RELATIVEDIR+PDF_NAME;
+			outname = outname.replace("$", ""+dentistid);
+			JasperExportManager.exportReportToPdfFile(jprint, outname);
+			System.out.println("CREATED REPORT "+outname);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
