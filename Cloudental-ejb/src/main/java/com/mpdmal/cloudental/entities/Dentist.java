a70cf62db0c86c516a64405f66d67c7a6773f34b
arilou <arilou@MePeeCee>
2012-11-05 21:10:41 +0200

    reception ui and user preferences

--- Cloudental-ejb/src/main/java/com/mpdmal/cloudental/entities/Dentist.java
+++ Cloudental-ejb/src/main/java/com/mpdmal/cloudental/entities/Dentist.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 public class Dentist extends com.mpdmal.cloudental.entities.base.DBEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	//Note, there is no point in carrying the dentist prefs around
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(unique=true)
