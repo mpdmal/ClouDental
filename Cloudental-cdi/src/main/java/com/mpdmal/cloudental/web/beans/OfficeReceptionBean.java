daae0b46a8e4949494e43337b2bf6765a7f7259a
arilou <arilou@MePeeCee>
2012-11-07 23:38:39 +0200

    browser tab title for reception

--- Cloudental-cdi/src/main/java/com/mpdmal/cloudental/web/beans/OfficeReceptionBean.java
+++ Cloudental-cdi/src/main/java/com/mpdmal/cloudental/web/beans/OfficeReceptionBean.java
@@ -1,30 +1,51 @@
 package com.mpdmal.cloudental.web.beans;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.mpdmal.cloudental.beans.DentistServices;
+import com.mpdmal.cloudental.entities.Dentist;
+import com.mpdmal.cloudental.util.exception.DentistNotFoundException;
 import com.mpdmal.cloudental.web.beans.base.BaseBean;
 
 @Named("officeReception")
 @SessionScoped
 public class OfficeReceptionBean extends BaseBean implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private int userID;
 	private boolean loggedIn = false;
+	private Dentist _dentist = null;
 	
+	//CDI BEANS
+	@Inject
+	private DentistServices _dsvc;
+	private String _receptionmsg;
 	public OfficeReceptionBean() {
 		super();
 		_baseName = "OfficeReception";
 	}
 	//GETTERS/SETTERS
+	public Dentist getUser() { return _dentist; }
 	public boolean isLoggedIn() {	return loggedIn;	}
-	public int getUserID() {	return userID;	}
+	public int getUserID() {	return _dentist.getId();	}
 
+	public String getReceptionMessage() { 
+		return (_dentist == null) ? "My office" : _receptionmsg;
+	}
 	public void setUserID(int userID) {	
-		this.userID = userID;
-		loggedIn = true;
+		try {
+			_dentist = _dsvc.findDentist(userID);
+			_receptionmsg = _dentist.getUIFriendlyString();
+			loggedIn = true;
+		} catch (DentistNotFoundException e) {
+			loggedIn = false;
+			e.printStackTrace();
+		}
 	}
 }
 
